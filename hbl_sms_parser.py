a24746d6c1c3a2a94a4330bcb733d1b92d7d099a
Salman Ahmed <ssahmed532@gmail.com>
2024-01-07 10:58:29 +0500

    1) lots of code cleanup 2) implemented the --list-all-vendors CLI option

--- hbl_sms_parser.py
+++ hbl_sms_parser.py
@@ -8,16 +8,21 @@ class HBLSmsParser:
     # SMS messages from these short codes will be assumed to be from HBL
     HBL_SHORT_CODES = ["4250"]
 
+    HBL_CC_TXN_RE = r"Dear Customer, Your HBL CreditCard \(ending with (?P<last4digits>\d{4})\) has been charged at (?P<vendor>.*) for (?P<txnamount>.*) on (?P<txndate>.*)"
+    HBL_CC_TXN_PTTRN = re.compile(HBL_CC_TXN_RE)
+
     def __init__(self):
         self.xml_tree = None
         self.xml_root = None
         self.all_vendors = set()
         self.msgs_parsed = 0
 
-    def _isSmsFromHBL(self, sms):
+    @staticmethod
+    def _isSmsFromHBL(sms):
         return sms.attrib["address"] in HBLSmsParser.HBL_SHORT_CODES
 
-    def _isMsgCreditCardTxn(self, sms):
+    @staticmethod
+    def _isMsgCreditCardTxn(sms):
         msg_body = sms.attrib["body"]
 
         if ("CreditCard" in msg_body) and ("has been charged at" in msg_body):
@@ -25,7 +30,8 @@ class HBLSmsParser:
 
         return False
 
-    def _extractDetailsFromTxnMsg(self, sms):
+    @staticmethod
+    def _extractDetailsFromTxnMsg(sms):
         ccTxnDetails = None
 
         ccLast4Digits = "N/A"
@@ -33,13 +39,10 @@ class HBLSmsParser:
         txnAmount = "N/A"
         txnDate = "N/A"
 
-        RE_HBL_CC_TXN = r"Dear Customer, Your HBL CreditCard \(ending with (?P<last4digits>\d{4})\) has been charged at (?P<vendor>.*) for (?P<txnamount>.*) on (?P<txndate>.*)"
-
-        p = re.compile(RE_HBL_CC_TXN)
-        m = p.match(sms.attrib["body"])
+        m = HBLSmsParser.HBL_CC_TXN_PTTRN.match(sms.attrib["body"])
         if m:
             assert len(m.groupdict()) == 4
-            print(m.groupdict())
+            # print(m.groupdict())
 
             ccTxnDetails = CreditCardTxn(
                 txnAmount=m.group("txnamount").strip(),
@@ -52,31 +55,41 @@ class HBLSmsParser:
 
         return ccTxnDetails
 
-    def _printSmsBody(self, sms):
+    @staticmethod
+    def _printSmsBody(sms):
         print(sms.attrib["body"])
         print()
 
     def loadFromSmsBackupFile(self, backupFilepath):
         self.xml_tree = ET.parse(backupFilepath)
         self.xml_root = self.xml_tree.getroot()
-        print(type(self.xml_tree))
-        print(type(self.xml_root))
+        # print(type(self.xml_tree))
+        # print(type(self.xml_root))
 
     def printAllVendors(self):
         print(f"Found {len(self.all_vendors)} unique Vendors from parsed SMS messages:")
         print()
-        for index, vendor in enumerate(self.all_vendors):
+        for index, vendor in enumerate(self.all_vendors, start=1):
             print(f"Vendor {index}: [{vendor}]")
 
     def parseMessages(self):
+        """Parse all SMS messages from the XML tree and build an internal
+           'representation' (store) of the all the messages.
+
+        Returns:
+            int: number of SMS messages parsed successfully
+        """
         count = 0
         for child in self.xml_root:
+            # TODO:
+            # add a check/filter for MMS; we aren't interested in
+            # parsing & processing MMS messages at the moment
             # print(child.tag, child.attrib)
             if self._isSmsFromHBL(child) and self._isMsgCreditCardTxn(child):
                 # print(f'Msg # {idx}, tag value: {child.tag}, attrib(s): {child.attrib}')
                 # print()
-                print(child)
-                self._printSmsBody(child)
+                # print(child)
+                # self._printSmsBody(child)
                 ccTxnDetails = self._extractDetailsFromTxnMsg(child)
                 if ccTxnDetails:
                     assert ccTxnDetails.txnAmount
@@ -86,8 +99,6 @@ class HBLSmsParser:
                     count += 1
 
                     self.all_vendors.add(ccTxnDetails.vendor)
-                    print(ccTxnDetails)
+                    # print(ccTxnDetails)
 
         return count
-
-
--- hbl_sms_tool.py
+++ hbl_sms_tool.py
@@ -1,19 +1,43 @@
-from collections import namedtuple
-import re
+from enum import StrEnum
+from time import perf_counter
 import sys
-import xml.etree.ElementTree as ET
 
 from hbl_sms_parser import HBLSmsParser
 
 
-CreditCardTxn = namedtuple(
-    "CreditCardTxn", ["txnAmount", "txnDate", "vendor", "ccLastFourDigits"]
-)
+class Command(StrEnum):
+    LIST_ALL_VENDORS = "--list-all-vendors"
+    PRINT_CC_TXNS = "--print-cc-txns"
+
+
+USAGE_MSG = "Usage: python hbl_sms_tool.py </path/to/SMS backup file.xml> [--list-all-vendors | --print-cc-txns]"
+
+ALL_COMMANDS = [cmd.value for cmd in Command]
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print(USAGE_MSG, file=sys.stderr)
+        sys.exit(1)
+
+    smsBackupFilepath = sys.argv[1]
+
+    if sys.argv[2] not in ALL_COMMANDS:
+        print(USAGE_MSG, file=sys.stderr)
+        sys.exit(1)
+    command = sys.argv[2]
+
     smsParser = HBLSmsParser()
+    time_start = perf_counter()
     smsParser.loadFromSmsBackupFile(sys.argv[1])
     msgCount = smsParser.parseMessages()
+    time_end = perf_counter()
+    print(f"Total elapsed time: {time_end - time_start} seconds")
     print(f"Parsed {msgCount} SMS messages from HBL")
-    smsParser.printAllVendors()
+    if command == Command.LIST_ALL_VENDORS:
+        smsParser.printAllVendors()
+    elif command == Command.PRINT_CC_TXNS:
+        smsParser.printCCTxns()
+    else:
+        print("Ooops!", file=sys.stderr)
+        sys.exit(2)
